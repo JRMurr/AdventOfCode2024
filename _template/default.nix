e4d3500db0c6eb3e6c6d4b0abd9caad11cab03fe
John Murray <5672686+JRMurr@users.noreply.github.com>
2024-12-04 22:51:53 -0500

    update template to include filePath

--- _template/default.nix
+++ _template/default.nix
@@ -3,16 +3,21 @@ let
 
   lib = pkgs.lib;
 
-  part0 = text: "TODO P1";
+  part0 = { text, filePath }: "TODO P1";
 
-  part1 = text: "TODO P2";
+  part1 = { text, filePath }: "TODO P2";
 
-  solve = text: {
-    "0" = part0 text;
-    "1" = part1 text;
-  };
+  solve = filePath:
+    let
+      text = builtins.readFile filePath;
+      attrs = { inherit text filePath; };
+    in
+    {
+      "0" = part0 attrs;
+      "1" = part1 attrs;
+    };
 in
 {
-  example = solve (builtins.readFile ./in.example);
-  real = solve (builtins.readFile ./in);
+  example = solve ./in.example);
+  real = solve ./in;
 }
